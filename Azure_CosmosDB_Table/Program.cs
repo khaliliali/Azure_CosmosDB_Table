7a5b7d850db817d639c3fa898ac5a6ff69cd8705
khaliliali <ali.khalili@outlook.com>
2023-06-03 19:23:57 +0800

    Adding Entity to CosmosDB Table

--- Azure_CosmosDB_Table/Program.cs
+++ Azure_CosmosDB_Table/Program.cs
@@ -1,24 +1,25 @@
 ﻿using Azure.Data.Tables;
 
-string connectionString = "";
-string tableName = "Orders";
+//string connectionString = "";
+//string tableName = "Orders";
 
 /* Adding Entity to CosmosDB Table */
 
-AddCosmosDBEntity("01", "Mobile", 123);
-AddCosmosDBEntity("02", "Laptop", 23);
-AddCosmosDBEntity("03", "Desktop", 123);
-AddCosmosDBEntity("04", "Laptop", 13);
-AddCosmosDBEntity("05", "Mobile", 33);
+//AddCosmosDBEntity("01", "Mobile", 123);
+//AddCosmosDBEntity("02", "Laptop", 23);
+//AddCosmosDBEntity("03", "Desktop", 123);
+//AddCosmosDBEntity("04", "Laptop", 13);
+//AddCosmosDBEntity("05", "Mobile", 33);
 
-void AddCosmosDBEntity(string orderID, string category, int quantity)
-{
-    TableClient tableClient = new TableClient(connectionString, tableName);
+//void AddCosmosDBEntity(string orderID, string category, int quantity)
+//{
+//    TableClient tableClient = new TableClient(connectionString, tableName);
 
-    TableEntity tableEntity = new TableEntity(category, orderID)
-    {
-        {"quantity", quantity }
-    };
-    tableClient.AddEntity(tableEntity);
-    Console.WriteLine("Added Entity order id {0}", orderID);
-}
+//    TableEntity tableEntity = new TableEntity(category, orderID)
+//    {
+//        {"quantity", quantity }
+//    };
+//    tableClient.AddEntity(tableEntity);
+//    Console.WriteLine("Added Entity order id {0}", orderID);
+//}
+
