bd2ffb72b449a898c3db52330e0a3754366c5a7d
khaliliali <ali.khalili@outlook.com>
2023-06-03 16:59:38 +0800

    Initial Commit

---
+++ Azure_CosmosDB_Table/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+string connectionString = "";
+string tableName = "Orders";
+
+/* Adding Entity to Table Storage */
+
+AddEntity("01", "Mobile", 123);
+AddEntity("02", "Laptop", 23);
+AddEntity("03", "Desktop", 123);
+AddEntity("04", "Laptop", 13);
+AddEntity("05", "Mobile", 33);
+
+void AddEntity(string orderID, string category, int quantity)
+{
+    TableClient tableClient = new TableClient(connectionString, tableName);
+
+    TableEntity tableEntity = new TableEntity(category, orderID)
+    {
+        {"quantity", quantity }
+    };
+    tableClient.AddEntity(tableEntity);
+    Console.WriteLine("Added Entity order id {0}", orderID);
+}
